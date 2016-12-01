a6925b69d3760514987ec82b4879b7e25d82759f
CATHRIN Allan <allan.cathrin@orange.fr>
2016-12-01 23:45:02 +0100

    Modification pour la durée de cookie et ajout de requête SQL

--- process_connexion.php
+++ process_connexion.php
@@ -16,20 +16,43 @@
 		header('Location:error.php');
 	}
 	
-	//Initialise une suite de caractère qui sera par la suite mélanger
-	$char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';	
+	try
+	{
+			//A utiliser plus tard - Mot de passe à remplir
+			//$bdd = new PDO('mysql:host=94.177.243.176;dbname=refuge;charset=utf8','root','lesnimphos973'); 
+			
+			//Connexion non généraliser
+			$bdd = new PDO('mysql:host=127.0.0.1;dbname=refuge;charset=utf8', 'root', ''); 
+	}
+	catch (Exception $e)
+	{
+			die('Erreur : ' . $e->getMessage());
+	}
 	
-	$create_id = (string) str_shuffle($char);//Mélange les caractères
-	$create_id = (string) substr($create_id, 1, 24);//Prend 24 caractères	
+	$req = $bdd->prepare('INSERT INTO user(pseudo,ville,point) VALUES (\''. $pseudo . '\',\'' .  $ville . '\',0);');
+	$req->execute();
+	$req->closeCursor();
 	
+	$req = $bdd->prepare('SELECT id FROM user WHERE ville=\''. $ville .'\' AND pseudo=\''. $pseudo . '\';');
+	$req->execute();
+	
+	while($donnees = $req->fetch())
+	{
+		$id =  $donnees['id'];
+	}
+	
+	$req->closeCursor();
+	
+	$int=3600*24*30*12;//Durée de presque 1 ans
+	
+	//Affecte les valeurs passer en POST dans des variables
 	$pseudo = $_POST['pseudo'];
 	$ville = $_POST['ville'];
 	
-	$int=99*12*30*86400;//Durée de 99 ans
+	//Creation des cookies pour la prochaine connexion de la personne sur le site
+	setcookie("pseudo",$pseudo,(time()+$int));
+	setcookie("ville",$ville,(time()+$int));
+	setcookie("id",$id,(time()+$int));
 	
-	setcookie('pseudo',$pseudo,(time()+$int));
-	setcookie('ville',$ville,(time()+$int));
-	setcookie('id',$create_id,(time()+$int));
-	
-	header('Location:index.php');
+	header('Location:test.php');
 ?>
