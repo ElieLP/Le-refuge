f00e304633ad011ef0c05c84f92b8d2618938fd7
DROUVIN Mathieu <mathieu.drouvin@etud.u-picardie.fr>
2016-12-01 22:44:32 +0100

    ajout css et accueil

---
+++ accueil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8"/>
+		<link rel=stylesheet href='css/reset.css' />
+		<link rel=stylesheet href='css/simplegrid.css' />
+		<link rel=stylesheet href='css/base.css' />
+		
+		<title>Les Nymphormaticiens</title>
+	</head>
+	<body>
+		<header>	
+			<div class="logo">
+				Le Refuge 
+			</div>
+			<h1>Page d'accueil</h1>
+		</header>
+		
+		<main>
+		<!--<form method="POST" action "pageprincipale.php">
+		<div class="central">
+			<label for="pseudo"> pseudo</label><input type="text" name="pseudo"/>
+			<label for="ville"> ville</label><input type="text" name="ville"/>
+			<input class="inputsub" type=submit value="connexion"/>
+		</div>
+		</form>-->
+
+		</main>
+		
+		<footer>
+			<div class="membres">
+				• Morgan Cozic • Matéo Damette • Elie Guedj • Clément Fournier • Antoine Duhamel • Emeric Demaret • Rémi Sid-Abdelkader • Mathieu Drouvin • Eymeric Deville • Allan Cathrin • Baptiste Mille •
+			</div>
+		</footer>
+	</body>
+</html>
