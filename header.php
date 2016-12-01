1c02c1579639bccb406d33ff00779e975a802447
Timothy HOUGUET <emeric.demaret@etud.u-picardie.fr>
2016-12-02 00:39:02 +0100

    reduction taille logo

---
+++ header.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="fontawesome/css/font-awesome.css"> 
+<header>
+	<a href="index.php"><img src="./logo.png"></a>
+
+	<nav>
+		<ul>
+		<div class="menu">
+			<li><a href="header.php?nom_categorie=vital">Vital</a></li>
+			<li><a href="header.php?nom_categorie=administration">Administration</a></li>
+			<li><a href="header.php?nom_categorie=soutien">Soutien</a></li>
+		</div>
+		</ul>
+	</nav>
+	<a href="ajout.php"><i class="fa fa-plus" aria-hidden="true"></i></a>
+</header>
