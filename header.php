61a8187f843a4b727dc10a53922b27537404c79e
CATHRIN Allan <allan.cathrin@orange.fr>
2016-12-02 06:23:31 +0100

    ajout

--- header.php
+++ header.php
@@ -5,26 +5,26 @@
 
 	<nav>
 		<ul>
-				<li><a href="header.php?nom_categorie=vital">Vital</a>
+				<li><a href="index.php?nom_categorie=vital">Vital</a>
 					<ul>
-						<li><a href="header.php?nom_categorie=alimentation">Alimentation</a></li>
-						<li><a href="header.php?nom_categorie=hygiene">Hygiène</a></li>
-						<li><a href="header.php?nom_categorie=logement">Logement</a></li>
-						<li><a href="header.php?nom_categorie=medical">Médical</a></li>
+						<li><a href="index.php?nom_categorie=alimentation">Alimentation</a></li>
+						<li><a href="index.php?nom_categorie=hygiene">Hygiène</a></li>
+						<li><a href="index.php?nom_categorie=logement">Logement</a></li>
+						<li><a href="index.php?nom_categorie=medical">Médical</a></li>
 					</ul>
 				</li>
-				<li><a href="header.php?nom_categorie=administration">Administration</a>
+				<li><a href="index.php?nom_categorie=administration">Administration</a>
 					<ul>
-						<li><a href="header.php?nom_categorie=demarches">Démarches Administratives</a></li>
-						<li><a href="header.php?nom_categorie=juridiques">Juridiques</a></li>
+						<li><a href="index.php?nom_categorie=demarches">Démarches Administratives</a></li>
+						<li><a href="index.php?nom_categorie=juridiques">Juridiques</a></li>
 					</ul>
 				</li>
-				<li><a href="header.php?nom_categorie=soutien">Soutien</a>
+				<li><a href="index.php?nom_categorie=soutien">Soutien</a>
 					<ul>
-						<li><a href="header.php?nom_categorie=psychologie">Psychologie</a></li>
-						<li><a href="header.php?nom_categorie=social">Social</a></li>
-						<li><a href="header.php?nom_categorie=association">Association Médical</a></li>
-						<li><a href="header.php?nom_categorie=dons">Dons Matériels</a></li>
+						<li><a href="index.php?nom_categorie=psychologie">Psychologie</a></li>
+						<li><a href="index.php?nom_categorie=social">Social</a></li>
+						<li><a href="index.php?nom_categorie=association">Association Médical</a></li>
+						<li><a href="index.php?nom_categorie=dons">Dons Matériels</a></li>
 					</ul>
 				</li>
 			</ul>
